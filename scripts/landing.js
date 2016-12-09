242ba6a952fa919524f4daddd8170040429e629f
Florence <roguegoatstudios@outlook.com>
2016-12-08 20:33:55 -0600

    checkpoint 8 quick edit

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,7 @@
  var animatePoints = function() {
  
      var points = document.getElementsByClassName('point');
- 
+     
      var revealFirstPoint = function() {
          points[0].style.opacity = 1;
          points[0].style.transform = "scaleX(1) translateY(0)";
@@ -22,6 +22,7 @@
          points[2].style.msTransform = "scaleX(1) translateY(0)";
          points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
      };
+     
  
      revealFirstPoint();
      revealSecondPoint();
