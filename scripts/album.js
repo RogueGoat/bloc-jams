049e79429823fae38fc209100771a5a3b3e5eae0
Florence <roguegoatstudios@outlook.com>
2017-01-07 12:43:03 -0600

    assignment 20 work complete

--- scripts/album.js
+++ scripts/album.js
@@ -44,7 +44,8 @@ var createSongRow = function(songNumber, songName, songLength) {
             currentSoundFile.play();
             $(this).html(pauseButtonTemplate);
             updatePlayerBarSong();
-        } else if (currentlyPlayingSongNumber === songNumber) {            if (currentSoundFile.isPaused()) {
+        } else if (currentlyPlayingSongNumber === songNumber) {            
+            if (currentSoundFile.isPaused()) {
              $(this).html(pauseButtonTemplate);
 $('.main-controls .play-pause').html(playerBarPauseButton);
     currentSoundFile.play();
@@ -182,6 +183,18 @@ var updatePlayerBarSong = function() {
 
 };
 
+var togglePlayFromPlayerBar = function(){
+    if(currentSoundFile.isPaused()){
+        getSongNumberCell(currentlyPlayingSongNumber).html(pauseButtonTemplate);
+        $playerPlayPause.html(playerBarPauseButton);
+        currentSoundFile.play();
+    } else if (currentSoundFile) {
+        getSongNumberCell(currentlyPlayingSongNumber).html(playButtonTemplate);
+        $playerPlayPause.html(playerBarPlayButton);
+        currentSoundFile.pause();
+    }
+};
+
  var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 
  var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
@@ -198,12 +211,13 @@ var updatePlayerBarSong = function() {
 
  var $previousButton = $('.main-controls .previous');
  var $nextButton = $('.main-controls .next');
-
+ var $playerPlayPause = $('.main-controls .play-pause');
 
  $(document).ready(function() {
     setCurrentAlbum(albumPicasso);
      
      $previousButton.click(previousSong);
      $nextButton.click(nextSong);
-
+     $playerPlayPause.click(togglePlayFromPlayerBar);
  });
+
